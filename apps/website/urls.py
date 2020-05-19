6b08559820e52b0134253b6c51a482e23599530f
Shubham Thakare <shubham.thakare@successive.tech>
2020-05-19 20:53:52 +0530

    Added index and other template pages

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('view/privacy-policy', views.privacy_policy, name='privacy_policy'),
+    path('view/terms-and-conditions',
+         views.terms_and_conditions,
+         name='terms_and_conditions'),
+    path('view/sitemap', views.sitemap, name='sitemap'),
 ]
--- apps/website/views.py
+++ apps/website/views.py
@@ -6,3 +6,15 @@ def index(request):
         'message': 'Hello, this is a demo page.',
     }
     return render(request, 'website/index.html', context)
+
+
+def privacy_policy(request):
+    return render(request, 'website/privacy_policy.html', None)
+
+
+def terms_and_conditions(request):
+    return render(request, 'website/terms_and_conditions.html', None)
+
+
+def sitemap(request):
+    return render(request, 'website/sitemap.html', None)
